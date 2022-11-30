0d7d477cb558d02cc2f2a09a73a6273aba03987c
akshit-khandelwal47 <akshitkhandelwal20@gmail.com>
2022-11-30 23:10:29 +0530

    interface

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-from flask import Flask
+from flask import Flask,request,url_for,redirect,render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("forest_fire.html")
