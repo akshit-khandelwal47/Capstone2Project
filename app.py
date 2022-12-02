22920008722dfc3589422751a430c7915d5f5f42
akshit-khandelwal47 <akshitkhandelwal20@gmail.com>
2022-12-02 22:05:34 +0530

    updated

--- app.py
+++ app.py
@@ -20,17 +20,17 @@ def predict():
     # output='{0:.{1}f}'.format(prediction[0][1], 2)
     
     lat = request.form.get("t1",type=float,default=0)
-    lon = request.form.get("t1",type=float,default=0)
-    bright = request.form.get("t1",type=float,default=0)
-    satellite = request.form.get("t1",type=float,default=0)
-    frp = request.form.get("t1",type=float,default=0)
-    dayn = request.form.get("t1",type=float,default=0)
-    type2 = request.form.get("t1",type=float,default=0)
-    type3 = request.form.get("t1",type=float,default=0)
-    scan = request.form.get("t1",type=float,default=0)
-    year = request.form.get("t1",type=float,default=0)
-    month = request.form.get("t1",type=float,default=0)
-    day = request.form.get("t1",type=float,default=0)
+    lon = request.form.get("t2",type=float,default=0)
+    bright = request.form.get("t3",type=float,default=0)
+    satellite = request.form.get("t4",type=float,default=0)
+    frp = request.form.get("t5",type=float,default=0)
+    dayn = request.form.get("t6",type=float,default=0)
+    type2 = request.form.get("t7",type=float,default=0)
+    type3 = request.form.get("t8",type=float,default=0)
+    scan = request.form.get("t9",type=float,default=0)
+    year = request.form.get("t10",type=float,default=0)
+    month = request.form.get("t11",type=float,default=0)
+    day = request.form.get("t12",type=float,default=0)
     
     # features = [float(i) for i in request.form.values()]
     # Convert features to array
