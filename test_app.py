d174c91886811ed990487d4d6465b78cb388a030
akshit-khandelwal47 <akshitkhandelwal20@gmail.com>
2022-12-02 22:18:39 +0530

    testing

---
+++ test_app.py
@@ -0,0 +1,12 @@
+from temp import *
+def test_add():
+    assert add(2,3) == 5
+
+def test_subtract():
+    assert subtract(5,1) == 4
+
+def test_multiply():
+    assert multiply(3,1) == 3
+
+def test_divide():
+    assert divide(10,5) == 2
